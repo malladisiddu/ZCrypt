1bdae1c007962a4e79843560b6d23c981717f47e
Malladi Siddartha <46401317+malladisiddu@users.noreply.github.com>
2020-07-17 22:00:03 +0530

    Add files via upload

---
+++ Attacks/RSA4.py
@@ -0,0 +1,17 @@
+from banner import *
+from utils import Convert
+banner()
+
+try:
+    n = int(input("==> n = "))
+    d = int(input("==> d = "))
+    c = int(input("==> c = "))
+    
+    decrypt = pow(c,d,n)
+    Convert(decrypt)
+except ValueError:
+    slowprint("\n[-] c,n,d Must be Integer Number")
+except AssertionError:
+    slowprint("\n[-] Wrong Data")
+except KeyboardInterrupt:
+    exit()
