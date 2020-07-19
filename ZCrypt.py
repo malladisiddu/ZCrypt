2f6d9163cde3fab73b07fdd9fc66180fabd9315d
Malladi Siddartha <46401317+malladisiddu@users.noreply.github.com>
2020-07-19 04:49:57 -0400

    Update ZCrypt.py

--- ZCrypt.py
+++ ZCrypt.py
@@ -2,9 +2,27 @@ from banner import *
 banner()
 print("""
 [1] - RSA
+[2] - XOR
+[0] - Exit
 """)
 check = int(input("==> "))
-if check == 1:
+if check == 2:
+    banner()
+    print("""
+[1] - Single Byte XOR
+[2] - Repeating Key XOR
+[0] - Exit
+     """)
+    y = int(input("==> "))
+    if y == 1:
+    	os.system(clear)
+    	import XOR.bruteforce
+    elif y == 2:
+    	os.system(clear)
+    	import XOR.repeating_key
+    else:
+    	exit()
+elif check == 1:
     banner()
     print("""
 [1] - (c,n,e)
@@ -15,23 +33,23 @@ if check == 1:
 [6] - (c,e)                 [Small Modulus Attack]
 [0] - Exit
 	""")
-    x = int(input("==> "))
-    if x == 1:
+    z = int(input("==> "))
+    if z == 1:
         os.system(clear)
         import RSA.RSA1
-    elif x == 2:
+    elif z == 2:
         os.system(clear)
         import RSA.RSA2
-    elif x == 3:
+    elif z == 3:
         os.system(clear)
         import RSA.RSA3
-    elif x == 4:
+    elif z == 4:
         os.system(clear)
         import RSA.RSA4
-    elif x == 5:
+    elif z == 5:
         os.system(clear)
         import RSA.RSA5
-    elif x == 6:
+    elif z == 6:
         os.system(clear)
         import RSA.RSA7
     else:
